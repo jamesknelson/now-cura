e4cd59600677e9185952027786328567c1a61652
James K Nelson <james@jamesknelson.com>
2019-11-06 17:18:49 +0900

    initial commit, fork of @now/static-build

---
+++ src/frameworks.ts
@@ -0,0 +1,320 @@
+import { readdir, stat, readFile, unlink } from 'fs';
+import { promisify } from 'util';
+import { join } from 'path';
+import { Route } from '@now/build-utils';
+import { injectGatsbyConfig } from './utils/gatsby-config';
+
+const readirPromise = promisify(readdir);
+const readFilePromise = promisify(readFile);
+const statPromise = promisify(stat);
+const unlinkPromise = promisify(unlink);
+const isDir = async (file: string): Promise<boolean> =>
+  (await statPromise(file)).isDirectory();
+
+// Please note that is extremely important
+// that the `dependency` property needs
+// to reference a CLI. This is needed because
+// you might want (for example) a Gatsby
+// site that is powered by Preact, so you
+// can't look for the `preact` dependency.
+// Instead, you need to look for `preact-cli`
+// when optimizing Preact CLI projects.
+
+export const frameworks: Framework[] = [
+  {
+    name: 'Gatsby.js',
+    dependency: 'gatsby',
+    getOutputDirName: async () => 'public',
+    defaultRoutes: async (dirPrefix: string) => {
+      try {
+        const nowRoutesPath = join(
+          dirPrefix,
+          'public',
+          '__now_routes_g4t5bY.json'
+        );
+        const content = await readFilePromise(nowRoutesPath, 'utf8');
+        const nowRoutes = JSON.parse(content);
+        try {
+          await unlinkPromise(nowRoutesPath);
+        } catch (err) {
+          // do nothing if deleting the file fails
+        }
+        return nowRoutes;
+      } catch (err) {
+        // if the file doesn't exist, we don't create routes
+        return [];
+      }
+    },
+    beforeBuildHook: async entrypointDir => {
+      await injectGatsbyConfig(entrypointDir);
+    },
+  },
+  {
+    name: 'Hexo',
+    dependency: 'hexo',
+    getOutputDirName: async () => 'public',
+  },
+  {
+    name: 'Eleventy',
+    dependency: '@11ty/eleventy',
+    getOutputDirName: async () => '_site',
+  },
+  {
+    name: 'Docusaurus 2.0',
+    dependency: '@docusaurus/core',
+    getOutputDirName: async () => 'build',
+  },
+  {
+    name: 'Preact',
+    dependency: 'preact-cli',
+    getOutputDirName: async () => 'build',
+    defaultRoutes: [
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '/(.*)',
+        dest: '/index.html',
+      },
+    ],
+  },
+  {
+    name: 'Ember',
+    dependency: 'ember-cli',
+    getOutputDirName: async () => 'dist',
+    defaultRoutes: [
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '/(.*)',
+        dest: '/index.html',
+      },
+    ],
+  },
+  {
+    name: 'Vue.js',
+    dependency: '@vue/cli-service',
+    getOutputDirName: async () => 'dist',
+    defaultRoutes: [
+      {
+        src: '^/[^/]*\\.(js|txt|ico|json)',
+        headers: { 'cache-control': 'max-age=300' },
+        continue: true,
+      },
+      {
+        src: '^/(img|js|css|fonts|media)/.*',
+        headers: { 'cache-control': 'max-age=31536000, immutable' },
+        continue: true,
+      },
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '^.*',
+        dest: '/index.html',
+      },
+    ],
+  },
+  {
+    name: 'Angular',
+    dependency: '@angular/cli',
+    minNodeRange: '10.x',
+    getOutputDirName: async (dirPrefix: string) => {
+      const base = 'dist';
+      const location = join(dirPrefix, base);
+      const content = await readirPromise(location);
+
+      // If there is only one file in it that is a dir we'll use it as dist dir
+      if (content.length === 1 && (await isDir(join(location, content[0])))) {
+        return join(base, content[0]);
+      }
+
+      return base;
+    },
+    defaultRoutes: [
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '/(.*)',
+        dest: '/index.html',
+      },
+    ],
+  },
+  {
+    name: 'Polymer',
+    dependency: 'polymer-cli',
+    getOutputDirName: async (dirPrefix: string) => {
+      const base = 'build';
+      const location = join(dirPrefix, base);
+      const content = await readirPromise(location);
+      const paths = content.filter(item => !item.includes('.'));
+
+      return join(base, paths[0]);
+    },
+    defaultRoutes: [
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '/(.*)',
+        dest: '/index.html',
+      },
+    ],
+  },
+  {
+    name: 'Svelte',
+    dependency: 'sirv-cli',
+    getOutputDirName: async () => 'public',
+    defaultRoutes: [
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '/(.*)',
+        dest: '/index.html',
+      },
+    ],
+  },
+  {
+    name: 'Create React App',
+    dependency: 'react-scripts',
+    getOutputDirName: async () => 'build',
+    defaultRoutes: [
+      {
+        src: '/static/(.*)',
+        headers: { 'cache-control': 's-maxage=31536000, immutable' },
+        continue: true,
+      },
+      {
+        src: '/service-worker.js',
+        headers: { 'cache-control': 's-maxage=0' },
+        continue: true,
+      },
+      {
+        src: '/sockjs-node/(.*)',
+        dest: '/sockjs-node/$1',
+      },
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '/(.*)',
+        headers: { 'cache-control': 's-maxage=0' },
+        dest: '/index.html',
+      },
+    ],
+  },
+  {
+    name: 'Create React App (ejected)',
+    dependency: 'react-dev-utils',
+    getOutputDirName: async () => 'build',
+    defaultRoutes: [
+      {
+        src: '/static/(.*)',
+        headers: { 'cache-control': 's-maxage=31536000, immutable' },
+        continue: true,
+      },
+      {
+        src: '/service-worker.js',
+        headers: { 'cache-control': 's-maxage=0' },
+        continue: true,
+      },
+      {
+        src: '/sockjs-node/(.*)',
+        dest: '/sockjs-node/$1',
+      },
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '/(.*)',
+        headers: { 'cache-control': 's-maxage=0' },
+        dest: '/index.html',
+      },
+    ],
+  },
+  {
+    name: 'Gridsome',
+    dependency: 'gridsome',
+    getOutputDirName: async () => 'dist',
+  },
+  {
+    name: 'UmiJS',
+    dependency: 'umi',
+    getOutputDirName: async () => 'dist',
+    defaultRoutes: [
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '/(.*)',
+        dest: '/index.html',
+      },
+    ],
+  },
+  {
+    name: 'Docusaurus 1.0',
+    dependency: 'docusaurus',
+    getOutputDirName: async (dirPrefix: string) => {
+      const base = 'build';
+      const location = join(dirPrefix, base);
+      const content = await readirPromise(location);
+
+      // If there is only one file in it that is a dir we'll use it as dist dir
+      if (content.length === 1 && (await isDir(join(location, content[0])))) {
+        return join(base, content[0]);
+      }
+
+      return base;
+    },
+  },
+  {
+    name: 'Sapper',
+    dependency: 'sapper',
+    getOutputDirName: async () => '__sapper__/export',
+  },
+  {
+    name: 'Saber',
+    dependency: 'saber',
+    getOutputDirName: async () => 'public',
+    defaultRoutes: [
+      {
+        src: '/_saber/.*',
+        headers: { 'cache-control': 'max-age=31536000, immutable' },
+      },
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '.*',
+        status: 404,
+        dest: '404.html',
+      },
+    ],
+  },
+  {
+    name: 'Stencil',
+    dependency: '@stencil/core',
+    getOutputDirName: async () => 'www',
+    defaultRoutes: [
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '/(.*)',
+        dest: '/index.html',
+      },
+    ],
+  },
+];
+
+export interface Framework {
+  name: string;
+  dependency?: string;
+  getOutputDirName: (dirPrefix: string) => Promise<string>;
+  defaultRoutes?: Route[] | ((dirPrefix: string) => Promise<Route[]>);
+  minNodeRange?: string;
+  beforeBuildHook?: (entrypointDir: string) => Promise<void>;
+}
---
+++ src/index.ts
@@ -0,0 +1,416 @@
+import ms from 'ms';
+import path from 'path';
+import spawn from 'cross-spawn';
+import getPort from 'get-port';
+import isPortReachable from 'is-port-reachable';
+import { ChildProcess, SpawnOptions } from 'child_process';
+import { existsSync, readFileSync, statSync, readdirSync } from 'fs';
+import { frameworks, Framework } from './frameworks';
+import {
+  glob,
+  download,
+  spawnAsync,
+  runNpmInstall,
+  runBundleInstall,
+  runPipInstall,
+  runPackageJsonScript,
+  runShellScript,
+  getNodeVersion,
+  getSpawnOptions,
+  Files,
+  Route,
+  BuildOptions,
+  Config,
+  debug,
+  PackageJson,
+  PrepareCacheOptions,
+} from '@now/build-utils';
+
+const sleep = (n: number) => new Promise(resolve => setTimeout(resolve, n));
+
+const DEV_SERVER_PORT_BIND_TIMEOUT = ms('5m');
+
+async function checkForPort(
+  port: number | undefined,
+  timeout: number
+): Promise<void> {
+  const start = Date.now();
+  while (!(await isPortReachable(port))) {
+    if (Date.now() - start > timeout) {
+      throw new Error(`Detecting port ${port} timed out after ${ms(timeout)}`);
+    }
+    await sleep(100);
+  }
+}
+
+function validateDistDir(
+  distDir: string,
+  isDev: boolean | undefined,
+  config: Config
+) {
+  const distDirName = path.basename(distDir);
+  const exists = () => existsSync(distDir);
+  const isDirectory = () => statSync(distDir).isDirectory();
+  const isEmpty = () => readdirSync(distDir).length === 0;
+
+  const hash = isDev
+    ? '#local-development'
+    : '#configuring-the-build-output-directory';
+  const docsUrl = `https://zeit.co/docs/v2/deployments/official-builders/static-build-now-static-build${hash}`;
+
+  const info = config.zeroConfig
+    ? '\nMore details: https://zeit.co/docs/v2/platform/frequently-asked-questions#missing-public-directory'
+    : `\nMake sure you configure the the correct distDir: ${docsUrl}`;
+
+  if (!exists()) {
+    throw new Error(`No output directory named "${distDirName}" found.${info}`);
+  }
+
+  if (!isDirectory()) {
+    throw new Error(
+      `Build failed because distDir is not a directory: "${distDirName}".${info}`
+    );
+  }
+
+  if (isEmpty()) {
+    throw new Error(
+      `Build failed because distDir is empty: "${distDirName}".${info}`
+    );
+  }
+}
+
+function getCommand(pkg: PackageJson, cmd: string, { zeroConfig }: Config) {
+  // The `dev` script can be `now dev`
+  const nowCmd = `now-${cmd}`;
+
+  if (!zeroConfig && cmd === 'dev') {
+    return nowCmd;
+  }
+
+  const scripts = (pkg && pkg.scripts) || {};
+
+  if (scripts[nowCmd]) {
+    return nowCmd;
+  }
+
+  if (scripts[cmd]) {
+    return cmd;
+  }
+
+  return zeroConfig ? cmd : nowCmd;
+}
+
+export const version = 2;
+
+const nowDevScriptPorts = new Map<string, number>();
+const nowDevChildProcesses = new Set<ChildProcess>();
+
+['SIGINT', 'SIGTERM'].forEach(signal => {
+  process.once(signal as NodeJS.Signals, () => {
+    for (const child of nowDevChildProcesses) {
+      debug(
+        `Got ${signal}, killing dev server child process (pid=${child.pid})`
+      );
+      process.kill(child.pid, signal);
+    }
+    process.exit(0);
+  });
+});
+
+const getDevRoute = (srcBase: string, devPort: number, route: Route) => {
+  const basic: Route = {
+    src: `${srcBase}${route.src}`,
+    dest: `http://localhost:${devPort}${route.dest}`,
+  };
+
+  if (route.headers) {
+    basic.headers = route.headers;
+  }
+
+  return basic;
+};
+
+async function getFrameworkRoutes(
+  framework: Framework,
+  dirPrefix: string
+): Promise<Route[]> {
+  if (!framework.defaultRoutes) {
+    return [];
+  }
+
+  let routes: Route[];
+
+  if (typeof framework.defaultRoutes === 'function') {
+    routes = await framework.defaultRoutes(dirPrefix);
+  } else {
+    routes = framework.defaultRoutes;
+  }
+
+  return routes;
+}
+
+export async function build({
+  files,
+  entrypoint,
+  workPath,
+  config,
+  meta = {},
+}: BuildOptions) {
+  debug('Downloading user files...');
+  await download(files, workPath, meta);
+
+  const mountpoint = path.dirname(entrypoint);
+  const entrypointDir = path.join(workPath, mountpoint);
+
+  let distPath = path.join(
+    workPath,
+    path.dirname(entrypoint),
+    (config && (config.distDir as string)) || 'dist'
+  );
+
+  const entrypointName = path.basename(entrypoint);
+
+  if (entrypointName === 'package.json') {
+    const pkgPath = path.join(workPath, entrypoint);
+    const pkg = JSON.parse(readFileSync(pkgPath, 'utf8')) as PackageJson;
+    const gemfilePath = path.join(workPath, 'Gemfile');
+    const requirementsPath = path.join(workPath, 'requirements.txt');
+
+    let output: Files = {};
+    let framework: Framework | undefined = undefined;
+    let minNodeRange: string | undefined = undefined;
+
+    const routes: Route[] = [];
+    const devScript = getCommand(pkg, 'dev', config as Config);
+
+    if (config.zeroConfig) {
+      if (existsSync(gemfilePath) && !meta.isDev) {
+        debug('Detected Gemfile, executing bundle install...');
+        await runBundleInstall(workPath, [], undefined, meta);
+      }
+      if (existsSync(requirementsPath) && !meta.isDev) {
+        debug('Detected requirements.txt, executing pip install...');
+        await runPipInstall(
+          workPath,
+          ['-r', requirementsPath],
+          undefined,
+          meta
+        );
+      }
+
+      const { HUGO_VERSION, ZOLA_VERSION, GUTENBERG_VERSION } = process.env;
+
+      if (HUGO_VERSION && !meta.isDev) {
+        console.log('Installing Hugo version ' + HUGO_VERSION);
+        const [major, minor] = HUGO_VERSION.split('.').map(Number);
+        const isOldVersion = major === 0 && minor < 43;
+        const prefix = isOldVersion ? `hugo_` : `hugo_extended_`;
+        const url = `https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${prefix}${HUGO_VERSION}_Linux-64bit.tar.gz`;
+        await spawnAsync(`curl -sSL ${url} | tar -zx -C /usr/local/bin`, [], {
+          shell: true,
+        });
+      }
+
+      if (ZOLA_VERSION && !meta.isDev) {
+        console.log('Installing Zola version ' + ZOLA_VERSION);
+        const url = `https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz`;
+        await spawnAsync(`curl -sSL ${url} | tar -zx -C /usr/local/bin`, [], {
+          shell: true,
+        });
+      }
+
+      if (GUTENBERG_VERSION && !meta.isDev) {
+        console.log('Installing Gutenberg version ' + GUTENBERG_VERSION);
+        const url = `https://github.com/getzola/zola/releases/download/v${GUTENBERG_VERSION}/gutenberg-v${GUTENBERG_VERSION}-x86_64-unknown-linux-gnu.tar.gz`;
+        await spawnAsync(`curl -sSL ${url} | tar -zx -C /usr/local/bin`, [], {
+          shell: true,
+        });
+      }
+
+      // `public` is the default for zero config
+      distPath = path.join(workPath, path.dirname(entrypoint), 'public');
+
+      const dependencies = Object.assign(
+        {},
+        pkg.dependencies,
+        pkg.devDependencies
+      );
+
+      framework = frameworks.find(
+        ({ dependency }) => dependencies[dependency || '']
+      );
+    }
+
+    if (framework) {
+      debug(
+        `Detected ${framework.name} framework. Optimizing your deployment...`
+      );
+
+      if (framework.minNodeRange) {
+        minNodeRange = framework.minNodeRange;
+        debug(
+          `${framework.name} requires Node.js ${framework.minNodeRange}. Switching...`
+        );
+      } else {
+        debug(
+          `${framework.name} does not require a specific Node.js version. Continuing ...`
+        );
+      }
+
+      if (framework.beforeBuildHook) {
+        await framework.beforeBuildHook(entrypointDir);
+      }
+    }
+
+    const nodeVersion = await getNodeVersion(
+      entrypointDir,
+      minNodeRange,
+      config
+    );
+    const spawnOpts = getSpawnOptions(meta, nodeVersion);
+
+    console.log('Installing dependencies...');
+    await runNpmInstall(entrypointDir, ['--prefer-offline'], spawnOpts, meta);
+
+    if (meta.isDev && pkg.scripts && pkg.scripts[devScript]) {
+      let devPort: number | undefined = nowDevScriptPorts.get(entrypoint);
+
+      if (typeof devPort === 'number') {
+        debug('`%s` server already running for %j', devScript, entrypoint);
+      } else {
+        // Run the `now-dev` or `dev` script out-of-bounds, since it is assumed that
+        // it will launch a dev server that never "completes"
+        devPort = await getPort();
+        nowDevScriptPorts.set(entrypoint, devPort);
+
+        const opts: SpawnOptions = {
+          cwd: entrypointDir,
+          stdio: 'inherit',
+          env: { ...process.env, PORT: String(devPort) },
+        };
+
+        const child: ChildProcess = spawn('yarn', ['run', devScript], opts);
+        child.on('exit', () => nowDevScriptPorts.delete(entrypoint));
+        nowDevChildProcesses.add(child);
+
+        // Now wait for the server to have listened on `$PORT`, after which we
+        // will ProxyPass any requests to that development server that come in
+        // for this builder.
+        try {
+          await checkForPort(devPort, DEV_SERVER_PORT_BIND_TIMEOUT);
+        } catch (err) {
+          throw new Error(
+            `Failed to detect a server running on port ${devPort}.\nDetails: https://err.sh/zeit/now/now-static-build-failed-to-detect-a-server`
+          );
+        }
+
+        debug('Detected dev server for %j', entrypoint);
+      }
+
+      let srcBase = mountpoint.replace(/^\.\/?/, '');
+
+      if (srcBase.length > 0) {
+        srcBase = `/${srcBase}`;
+      }
+
+      // We ignore defaultRoutes for `now dev`
+      // since in this case it will get proxied to
+      // a custom server we don't have control over
+      routes.push(
+        getDevRoute(srcBase, devPort, {
+          src: '/(.*)',
+          dest: '/$1',
+        })
+      );
+    } else {
+      if (meta.isDev) {
+        debug(`WARN: "${devScript}" script is missing from package.json`);
+        debug(
+          'See the local development docs: https://zeit.co/docs/v2/deployments/official-builders/static-build-now-static-build/#local-development'
+        );
+      }
+
+      const buildScript = getCommand(pkg, 'build', config as Config);
+      debug(`Running "${buildScript}" script in "${entrypoint}"`);
+
+      const found = await runPackageJsonScript(
+        entrypointDir,
+        buildScript,
+        spawnOpts
+      );
+
+      if (!found) {
+        throw new Error(
+          `Missing required "${buildScript}" script in "${entrypoint}"`
+        );
+      }
+
+      const outputDirPrefix = path.join(workPath, path.dirname(entrypoint));
+
+      if (framework) {
+        const outputDirName = await framework.getOutputDirName(outputDirPrefix);
+
+        distPath = path.join(outputDirPrefix, outputDirName);
+      } else if (!config || !config.distDir) {
+        // Select either `dist` or `public` as directory
+        const publicPath = path.join(entrypointDir, 'public');
+
+        if (
+          !existsSync(distPath) &&
+          existsSync(publicPath) &&
+          statSync(publicPath).isDirectory()
+        ) {
+          distPath = publicPath;
+        }
+      }
+
+      validateDistDir(distPath, meta.isDev, config);
+
+      if (framework) {
+        const frameworkRoutes = await getFrameworkRoutes(
+          framework,
+          outputDirPrefix
+        );
+        routes.push(...frameworkRoutes);
+      }
+
+      output = await glob('**', distPath, mountpoint);
+    }
+
+    const watch = [path.join(mountpoint.replace(/^\.\/?/, ''), '**/*')];
+    return { routes, watch, output, distPath };
+  }
+
+  if (!config.zeroConfig && entrypointName.endsWith('.sh')) {
+    debug(`Running build script "${entrypoint}"`);
+    const nodeVersion = await getNodeVersion(entrypointDir, undefined, config);
+    const spawnOpts = getSpawnOptions(meta, nodeVersion);
+    await runShellScript(path.join(workPath, entrypoint), [], spawnOpts);
+    validateDistDir(distPath, meta.isDev, config);
+
+    const output = await glob('**', distPath, mountpoint);
+
+    return {
+      output,
+      routes: [],
+      watch: [],
+      distPath,
+    };
+  }
+
+  let message = `Build "src" is "${entrypoint}" but expected "package.json"`;
+
+  if (!config.zeroConfig) {
+    message += ' or "build.sh"';
+  }
+
+  throw new Error(message);
+}
+
+export async function prepareCache({ workPath }: PrepareCacheOptions) {
+  return {
+    ...(await glob('node_modules/**', workPath)),
+    ...(await glob('package-lock.json', workPath)),
+    ...(await glob('yarn.lock', workPath)),
+  };
+}
